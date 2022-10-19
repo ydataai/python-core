aeb2622cac1d1d32af08a7386728dcfa9d37529f
Luís Portela Afonso <luis.portela@ydata.ai>
2022-10-19 18:26:43 +0100

    fix unit tests

--- src/core/tests/test_fabric_error.py
+++ src/core/tests/test_fabric_error.py
@@ -7,12 +7,14 @@ def test_basic_error():
   basic_error.name = "BasicError"
   basic_error.description = "This is a basic test error"
   basic_error.return_value = -1
+  basic_error.http_code = 500
 
   basic_error_dict = dict(basic_error)
 
   assert 'name' in basic_error_dict
   assert 'description' in basic_error_dict
   assert 'return_value' in basic_error_dict
+  assert 'http_code' in basic_error_dict
 
 def test_full_error():
   basic_error = FabricError(context={'a': 'a'}, http_code=500, name="FullError")
