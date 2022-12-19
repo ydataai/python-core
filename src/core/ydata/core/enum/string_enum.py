8e851f849b8ef8ab286a50f5365c97841ee72ac8
Luís Portela Afonso <luis.portela@ydata.ai>
2022-12-19 12:45:59 +0000

    feat(enum): add StringEnum to core

---
+++ src/core/ydata/core/enum/string_enum.py
@@ -0,0 +1,30 @@
+from enum import Enum
+
+
+class StringEnum(Enum):
+  @classmethod
+  def _missing_(cls, value):
+    if isinstance(value, str):
+      upper_value = value.upper()
+
+      key = StringEnum._key_from_str_(upper_value)
+      if key is not None:
+        return key
+
+      lower_value = value.lower()
+
+      key = StringEnum._key_from_str_(lower_value)
+      if key is not None:
+        return key
+
+    raise ValueError(f"{value} is not a valid {cls.__name__}")
+
+  @classmethod
+  def _key_from_str_(cls, value: str):
+    if value in cls.__members__:
+      return cls(value)
+
+    return None
+
+  def __eq__(self, o: object) -> bool:
+    return self.value == o.value
