e5a89c51b1a37f4836a5b09bf66fdc4c11defbdd
ricardodcpereira <ricardo.pereira@ydata.ai>
2023-03-23 11:24:10 +0000

    fix: lint error

--- src/core/ydata/core/enum/privacy.py
+++ src/core/ydata/core/enum/privacy.py
@@ -10,9 +10,8 @@ class PrivacyLevel(Enum):
     def __str__(self):
         if self.value == self.HIGH_FIDELITY.value:
             return "High Fidelity"
-        elif self.value == self.HIGH_PRIVACY.value:
+        if self.value == self.HIGH_PRIVACY.value:
             return "High Privacy"
-        elif self.value == self.BALANCED_PRIVACY_FIDELITY.value:
+        if self.value == self.BALANCED_PRIVACY_FIDELITY.value:
             return "Balanced Privacy/Fidelity"
-        else:
-            return "N/D"
+        return "N/D"
