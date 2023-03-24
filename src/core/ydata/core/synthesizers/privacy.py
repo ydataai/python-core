fd79d14b1437d3baebd774efee83b7ead187c254
ricardodcpereira <ricardo.pereira@ydata.ai>
2023-03-24 14:15:50 +0000

    feat: add documentation

--- src/core/ydata/core/synthesizers/privacy.py
+++ src/core/ydata/core/synthesizers/privacy.py
@@ -4,8 +4,11 @@ from enum import Enum, auto
 class PrivacyLevel(Enum):
     """Privacy level exposed to the end-user."""
     HIGH_FIDELITY = auto()
+    """High fidelity"""
     HIGH_PRIVACY = auto()
+    """High privacy"""
     BALANCED_PRIVACY_FIDELITY = auto()
+    """Balanced privacy/fidelity"""
 
     def __str__(self):
         if self.value == self.HIGH_FIDELITY.value:
